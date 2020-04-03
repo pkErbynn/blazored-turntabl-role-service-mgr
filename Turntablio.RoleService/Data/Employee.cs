8992d9824744ecaf667ab6e083cdf43cd947902e
erbynn <john.erbynn@gmail.com>
2020-04-03 18:56:31 +0100

    first commit

---
+++ Turntablio.RoleService/Data/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turntablio.RoleService.Data
+{
+    public class Employee
+    {
+        public int employeeid { get; set; } 
+        public string employeefname { get; set; }
+        public string employeelname { get; set; }
+        public string employeeemail { get; set; }
+        public string employeeaddress { get; set; }
+        public string employeerole { get; set; }
+    }
+}
---
+++ Turntablio.RoleService/Data/EmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace Turntablio.RoleService.Data
+{
+    public class EmployeeService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public EmployeeService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<Employee[]> GetEmplyeeAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+           "https://role-api.herokuapp.com/api/employees");
+            //request.Headers.Add("Accept", "application/vnd.github.v3+json");
+            //request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+
+            var client = _clientFactory.CreateClient();
+            var response = await client.SendAsync(request);
+
+            return null;
+
+        }
+    }
+}
