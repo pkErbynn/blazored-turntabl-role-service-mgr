499e1478e6fb82d54a5ba37e5ea3bce1dab40764
erbynn <john.erbynn@gmail.com>
2020-04-08 14:17:03 +0100

    wip: init service layer

---
+++ Turntablio.RoleService/Data/Service/EmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Turntablio.RoleService.Data.Model;
+
+namespace Turntablio.RoleService.Data.Service
+{
+    public class EmployeeService
+    {
+        private readonly EmployeeContext _dbContext;
+
+        public EmployeeService(EmployeeContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
