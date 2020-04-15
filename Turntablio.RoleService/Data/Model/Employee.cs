f1ba424fff8718133a7f7bffbb90fd02d8e0730b
erbynn <john.erbynn@gmail.com>
2020-04-15 13:05:28 +0100

    wip: udate emp form validation

--- Turntablio.RoleService/Data/Model/Employee.cs
+++ Turntablio.RoleService/Data/Model/Employee.cs
@@ -13,22 +13,23 @@ namespace Turntablio.RoleService.Data.Model
 
         [Required(ErrorMessage = "Please enter employee's first name")]
         [Display(Name = "First Name")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "First Name can't be more than 50 characters.")]
         public string EmployeeFirstName { get; set; }
 
         [Required(ErrorMessage = "Please enter employee's last name")]
         [Display(Name = "Last Name")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "Last name can't be more than 50 characters.")]
         public string EmployeeLastName { get; set; }
 
         [Required(ErrorMessage = "Please enter employee's email")]
+        [EmailAddress(ErrorMessage ="Please provide a valid e-mail address. eg. you@turntabl.io")]
         [Display(Name = "Email")]
-        [StringLength(60)]
+        [StringLength(60, ErrorMessage ="Email can't be more than 60 characters.")]
         public string EmployeeEmail { get; set; }
 
         [Required(ErrorMessage = "Please enter employee's address")]
         [Display(Name = "Address")]
-        [StringLength(255)]
+        [StringLength(255, ErrorMessage = "Address can't be more than 255 characters.")]
         public string EmployeeAddress { get; set; }
 
         public ICollection<EmployeeRole> EmployeeRoles { get; set; }
